095af1478f639511e2942169256e5886cde82b7c
Ye0ngjin <wkgek@naver.com>
2023-05-16 17:29:30 +0900

    일부수정

--- src/main/java/com/cos/blog/BlogApplication.java
+++ src/main/java/com/cos/blog/BlogApplication.java
@@ -2,8 +2,10 @@ package com.cos.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class BlogApplication {
 
 	public static void main(String[] args) {
