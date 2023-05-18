15fa1233d0d43360c42627ed9dfd26a9953a100b
Ye0ngjin <wkgek@naver.com>
2023-05-18 09:18:14 +0900

    now()->SYSDATE

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -12,14 +12,14 @@ import com.cos.blog.model.User;
 // 자동으로 bean등록이 된다.
 // @Repository // 생략 가능하다.
 public interface UserRepository extends JpaRepository<User, Integer>{
-	// SELECT * FROM user WHERE username = 1?;
+	// SELECT * FROM users WHERE username = 1?;
 	Optional<User> findByUsername(String username);
 }
 
 
 // JPA Naming 쿼리
-// SELECT * FROM user WHERE username = ?1 AND password = ?2;
+// SELECT * FROM users WHERE username = ?1 AND password = ?2;
 //User findByUsernameAndPassword(String username, String password);
 
-//	@Query(value="SELECT * FROM user WHERE username = ?1 AND password = ?2", nativeQuery = true)
+//	@Query(value="SELECT * FROM users WHERE username = ?1 AND password = ?2", nativeQuery = true)
 //	User login(String username, String password);
