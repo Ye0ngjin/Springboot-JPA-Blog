b6df177521da6d7be5bfa6dab3ac744d101196b8
Ye0ngjin <104483761+Ye0ngjin@users.noreply.github.com>
2023-10-30 23:09:59 +0900

    Update update_check.sh

--- update_check.sh
+++ update_check.sh
@@ -3,7 +3,7 @@
 # 파일에 대한 실행 권한을 부여하고
 # chmod +x update_check.sh
 # chmod +x auto_update.sh
-# 다음를 실행시켜서
+# 다음을 실행시켜서
 # crontab -e
 # 다음을 추가해서, 스케줄링해준다.
 # */10 * * * * /bin/bash ~/Springboot-JPA-Blog/update_check.sh > /dev/null 2>&1
@@ -26,6 +26,17 @@ then
   echo "$(date +'%Y-%m-%d-%H-%M-%S')	변화 없음" | tee -a target/nohup.out
 else
   # 변경 사항이 있는 경우 스크립트 재실행 또는 조치 수행
-  echo "$(date +'%Y-%m-%d-%H-%M-%S')	깃 수정됨. 서버를 다시 시작합니다." | tee -a target/nohup.out
-  ./auto_update.sh
+  echo "$(date +'%Y-%m-%d-%H-%M-%S')	깃 수정됨." | tee -a target/nohup.out
+  
+  # Git 변경 내용 확인
+  changes=$(git diff --name-only HEAD~1 HEAD)
+
+  # 변경 내용 중에서 src/main/로 시작하는 경로 확인
+  if echo "$changes" | grep -q '^src/main/'; then
+    echo "Changes found in src/main/ folder or its subdirectories." | tee -a target/nohup.out
+    # 스크립트 실행
+    /bin/bash auto_update.sh
+  else
+    echo "No changes found in src/main/ folder or its subdirectories." | tee -a target/nohup.out
+  fi
 fi
