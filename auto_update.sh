be1c8678122f7375f7ba595ccb0d795cd46ac1b2
Ye0ngjin <104483761+Ye0ngjin@users.noreply.github.com>
2023-10-31 16:40:51 +0900

    Update auto_update.sh

--- auto_update.sh
+++ auto_update.sh
@@ -2,8 +2,11 @@
 
 cd ~/Springboot-JPA-Blog
 
+SNAPSHOT="blog-0.0.1-SNAPSHOT"
+extension=".war"
+
 # 1. 현재 실행 중인 웹 애플리케이션 프로세스 종료, grep -v "grep"은 그랩 명령이 실행되는 프로세스가 나오는 것을 방지한다.
-PID=$(ps aux | grep "blog-0.0.1-SNAPSHOT" | grep -v "grep" | awk '{print $2}')
+PID=$(ps aux | grep "$SNAPSHOT" | grep -v "grep" | awk '{print $2}')
 if [ -n "$PID" ]; then
     echo "Stopping the existing web application process (PID: $PID)" | tee -a target/nohup.out
     kill -9 $PID
@@ -20,9 +23,9 @@ mvn package | tee -a "target/$LOGFILE"
 
 # 4. 실행 파일 권한 변경
 echo "Changing the execution permissions of the application..."
-chmod 775 target/blog-0.0.1-SNAPSHOT.jar | tee -a "target/$LOGFILE"
+chmod 775 target/$SNAPSHOT$extension | tee -a "target/$LOGFILE"
 
 # 5. 웹 애플리케이션 다시 실행
 cd target
 echo "Starting the web application..."| tee -a "$LOGFILE"
-nohup java -jar blog-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 &
+nohup java -jar $SNAPSHOT$extension > nohup.out 2>&1 &
