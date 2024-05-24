67cec2decf724139ec5890193fe4e05aad41b985
Ye0ngjin <104483761+Ye0ngjin@users.noreply.github.com>
2024-05-24 19:58:10 +0900

    Update backup_log.sh

--- backup_log.sh
+++ backup_log.sh
@@ -9,4 +9,4 @@ cp target/nohup.out "target/$LOGFILE"
 echo "Log file renamed to $LOGFILE from nohup.out" | tee -a "target/$LOGFILE"
 
 cat /dev/null > target/nohup.out
-echo "Log file was backed up at $TIME from nohup.out" | tee -a target/nohup
+echo "Log file was backed up at $TIME from nohup.out" | tee -a target/nohup.out
